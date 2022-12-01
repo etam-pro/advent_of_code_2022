d16043adcc8141f084700d4c3d34060337bd097e
Edward Tam <edward.tam@flipp.com>
2022-12-01 10:13:45 -0500

    Day 1 Part 2

--- day1/run.rb
+++ day1/run.rb
@@ -1,23 +1,49 @@
 require 'byebug'
 
-max = 0
-acc = 0
-
 # NOTE: remove \n
 def trim(line)
   line[0..-2]
 end
 
-File.open('day1/input').each do |line|
-  cal = trim(line)
+def part_1
+  max = 0
+  acc = 0
 
-  if cal == ''
-    max = [max, acc].max
-    acc = 0
-    next
+  File.open('day1/input').each do |line|
+    cal = trim(line)
+
+    if cal == ''
+      max = [max, acc].max
+      acc = 0
+      next
+    end
+
+    acc += cal.to_i
   end
 
-  acc += cal.to_i
+  puts "How much time does it take to burn #{max} Calories?"
 end
 
-puts "How much time does it take to burn #{max} Calories?"
+def part_2
+  acc = 0
+  cals = []
+
+  File.open('day1/input').each do |line|
+    cal = trim(line)
+
+    if cal == ''
+      cals << acc
+      acc = 0
+      next
+    end
+
+    acc += cal.to_i
+  end
+
+  total = cals.sort.reverse.first(3).reduce(:+)
+
+  puts "How much time does it take to burn #{total} Calories?"
+end
+
+part_1
+part_2
